f85acf92526ce3d67d5d170f3012bc40cba2c06c
Charles Eromose <okuekhamhene.gmail.com>
2025-04-01 17:24:57 +0100

    completed housing app

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from . models import *
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Property)
+# admin.site.register(UserListing)
+# admin.site.register(SingleListing)
+# admin.site.register(Listing)
+# admin.site.register(Order)
