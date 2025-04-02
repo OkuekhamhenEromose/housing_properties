99900b609126a2109ef0e9dbdd4bf9e5a954f000
Charles Eromose <okuekhamhene.gmail.com>
2025-04-02 20:22:13 +0100

    register media images

--- api/settings.py
+++ api/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/image/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'image')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
